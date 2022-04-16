b6d6b23b0ae3621e36bad225f6c57545e5ff41b4
zhuzunxiong <zhuzunxiong@bytedance.com>
2022-04-16 16:32:10 +0800

    添加发送方接收方昵称设置
    Change-Id: I5de25f03ab301eaba5253a7630c2f32433917fe2

--- mail/mail.go
+++ mail/mail.go
@@ -17,33 +17,32 @@ import (
 )
 
 type Header struct {
-	From        string `json:"From"`         //发送方名字
-	To          string `json:"To"`           //接收方邮箱
 	Subject     string `json:"Subject"`      //标题
 	ContentType string `json:"Content-Type"` //内容格式
 }
 
 type Email struct {
-	Host     string `json:"Host"`     //smtp服务器
-	Port     int    `json:"Port"`     //smtp服务器端口
-	Email    string `json:"Email"`    // 这里是你的邮箱地址
-	Password string `json:"Password"` // 这里填你的授权码
-	ToEmail  string `json:"ToEmail"`  // 目标地址
-	Header   Header `json:"Header"`
-	Body     string `json:"Body"` //邮件内容
+	Host       string `json:"Host"`       //smtp服务器
+	Port       int    `json:"Port"`       //smtp服务器端口
+	Email      string `json:"Email"`      // 这里是你的邮箱地址
+	NickName   string `json:"NickName"`   // 发送方昵称
+	Password   string `json:"Password"`   // 这里填你的授权码
+	ToEmail    string `json:"ToEmail"`    // 目标地址
+	ToNickName string `json:"ToNickName"` //接收方昵称
+	Header     Header `json:"Header"`
+	Body       string `json:"Body"` //邮件内容
 }
 
 func createEmailJson(src string) {
 	res := &Email{
-		Host:     "smtp.qq.com",  //smtp服务器
-		Port:     465,            //端口
-		Email:    "xxxx@xxx.xxx", //发送方的邮箱
-		Password: "xxx",          //发送方的密钥
-		ToEmail:  "xxxx@xxx.xxx", //接收方邮箱
+		Host:       "smtp.qq.com",  //smtp服务器
+		Port:       465,            //端口
+		Email:      "xxxx@xxx.xxx", //发送方的邮箱
+		NickName:   "xxx",
+		Password:   "xxx",          //发送方的密钥
+		ToEmail:    "xxxx@xxx.xxx", //接收方邮箱
+		ToNickName: "xxx",          //接收方昵称
 		Header: Header{
-			From: "xxxxx", //发送方昵称
-			To:   "xxxxx", //接收方邮箱
-
 			Subject:     "xxxxx",                    //邮件标题
 			ContentType: "text/html;chartset=UTF-8", //邮件格式
 		},
@@ -55,14 +54,14 @@ func createEmailJson(src string) {
 	fmt.Scanln(&res.Port)
 	fmt.Println("请输入发送方邮箱")
 	fmt.Scanln(&res.Email)
+	fmt.Println("请输入发送方昵称")
+	fmt.Scanln(&res.NickName)
 	fmt.Println("请输入发送方密钥")
 	fmt.Scanln(&res.Password)
 	fmt.Println(`请输入接收方邮箱`)
 	fmt.Scanln(&res.ToEmail)
-	fmt.Println("请输入发送方昵称")
-	fmt.Scanln(&res.Header.From)
-	res.Header.From = res.Header.From + "<" + res.Email + ">"
-	res.Header.To = res.ToEmail
+	fmt.Println("请输入接收方昵称")
+	fmt.Scanln(&res.ToNickName)
 	fmt.Println("请输入邮件标题")
 	fmt.Scanln(&res.Header.Subject)
 	fmt.Println(`请输入邮件格式（直接回车为默认"text/html;chartset=UTF-8"）`)
@@ -91,15 +90,15 @@ func MailTo(to, msg string) {
 		info.Body = msg
 	}
 	message := gomail.NewMessage()
-	message.SetHeader("From", info.Email)
-	message.SetHeader("To", info.ToEmail)
+	message.SetAddressHeader("From", info.Email, info.ToEmail)
+	message.SetAddressHeader("To", info.ToEmail, "")
 	//设置主体
 	message.SetHeader("Subject", info.Header.Subject)
 	message.SetHeader("ContentType", info.Header.ContentType)
 	//设置正文
 	message.SetBody("text/html", info.Body)
 
-	dialer := gomail.NewDialer(info.Host, info.Port, info.Header.From, info.Password)
+	dialer := gomail.NewDialer(info.Host, info.Port, info.Email, info.Password)
 
 	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
